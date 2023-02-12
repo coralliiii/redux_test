0d162fc1ea64dd24a6dc3d8fbccd6b7f16766a43
CoralLi <944284279@qq.com>
2023-02-12 20:30:33 +0800

    20230212

--- 5_src_react-redux的基本使用/components/Count/index.jsx
+++ 5_src_react-redux的基本使用/components/Count/index.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from 'react'
-//引入action
-import {
-	createIncrementAction,
-	createDecrementAction,
-	createIncrementAsyncAction
-} from '../../redux/count_action'
-//引入connect用于连接UI组件与redux
-import {connect} from 'react-redux'
 
-//定义UI组件
-class Count extends Component {
+export default class Count extends Component {
 
 	state = {carName:'奔驰c63'}
 
@@ -54,23 +45,3 @@ class Count extends Component {
 		)
 	}
 }
-
-//使用connect()()创建并暴露一个Count的容器组件
-export default connect(
-	state => ({count:state}),
-
-	//mapDispatchToProps的一般写法
-	/* dispatch => ({
-		jia:number => dispatch(createIncrementAction(number)),
-		jian:number => dispatch(createDecrementAction(number)),
-		jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time)),
-	}) */
-
-	//mapDispatchToProps的简写
-	{
-		jia:createIncrementAction,
-		jian:createDecrementAction,
-		jiaAsync:createIncrementAsyncAction,
-	}
-)(Count)
-
